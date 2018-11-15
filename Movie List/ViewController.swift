ebe8bbc8ea31835ef771b7087840e2da6c7b330f
李耀宗 <907146233@qq.com>
2018-11-15 15:48:46 +0800

    Initial Commit

---
+++ Movie List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Movie List
+//
+//  Created by 李耀宗 on 2018/11/15.
+//  Copyright © 2018年 LYZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
